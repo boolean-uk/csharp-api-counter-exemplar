6ed19b0dab5b068d88c76ce640246379c5d5de00
uerbzr <nigel.sibbert@gmail.com>
2023-09-25 14:33:26 +0100

    completed

--- api-counter/api-counter.wwwapi/Controllers/CounterController.cs
+++ api-counter/api-counter.wwwapi/Controllers/CounterController.cs
@@ -26,20 +26,22 @@ namespace api_counter.wwwapi.Controllers
 
         //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IResult> GetAllCounters()
         {
             //change the number returned in the line below to counter list variable
-            return Results.Ok(1);
+            return Results.Ok(counters);
         }
 
         //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
         [HttpGet]
         [Route("{id}")]
-        public async Task<IResult> GetACounter()
+        public async Task<IResult> GetACounter(int id)
         {
             //write code here replacing the string.Empty
-            var counter = string.Empty;
-           
+            var counter = counters.Where(counter => counter.Id == id).FirstOrDefault();
+
+
             //leave return line the same
             return counter != null ? Results.Ok(counter) : Results.NotFound();
         }
@@ -47,27 +49,63 @@ namespace api_counter.wwwapi.Controllers
         //TODO: 3.  write another controlller method that returns counters that have a value greater than the {number} passed in.
         // use method below as starting point
         [HttpGet]
-        [Route("greaterthan{number}")]
-        public async Task<IResult> Get()
+        [Route("greaterthan/{number}")]
+        public async Task<IResult> GetGreater(int number)
         {
-            return Results.Ok(counters);
+            var results = counters.Where(counter => counter.Value >= number).ToList();
+
+            return results.Count > 0 ? Results.Ok(counters) : Results.NotFound();
         }
 
         ////TODO:4. write another controlller method that returns counters that have a value less than the {number} passed in.
-      
-      
 
+        [HttpGet]
+        [Route("lessthan/{number}")]
+        public async Task<IResult> GetLess(int number)
+        {
+            var results = counters.Where(counter => counter.Value < number).ToList();
+
+            return results.Count > 0 ? Results.Ok(counters) : Results.NotFound();
+        }
 
 
         //Extension #1
         //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
         //return the counter you have increased
-        
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("increments/{id}")]
+        public async Task<IResult> Increase(int id)
+        {
+            var counter = counters.Where(counter => counter.Id == id).FirstOrDefault();
+            if (counter != null)
+            {
+                counter.Value++;
+                return Results.Ok(counter);
+            }
+            return Results.NotFound();
+        }
 
         //Extension #2
         //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
         //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
         //return the counter you have decreased
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("decrements/{id}")]
+        public async Task<IResult> Decrease(int id)
+        {
+            var counter = counters.Where(counter => counter.Id == id).FirstOrDefault();
+            if (counter != null)
+            {
+                counter.Value--;
+                return Results.Ok(counter);
+            }
+            return Results.NotFound();
+        }
     }
 }
